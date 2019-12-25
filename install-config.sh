07527be10bbc4caa17879c3ae4f1ffd54cc127fb
Nisheeth Lahoti <nisheeth.lahoti@gmail.com>
2019-12-25 12:27:52 +0530

    No need of neovim ppa anymore

--- install-config.sh
+++ install-config.sh
@@ -1,7 +1,6 @@
 git clone --bare --config status.showUntrackedFiles=no https://github.com/nisheethlahoti/dotfiles.git ~/.dotfiles.git
 git --git-dir=$HOME/.dotfiles.git/ --work-tree=$HOME checkout
 echo "source ~/.additional.bash.rc" >> ~/.bashrc
-sudo add-apt-repository -y ppa:neovim-ppa/stable
 sudo apt-get -y install neovim
 curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
 	https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
