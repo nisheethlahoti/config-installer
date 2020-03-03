35296745552d60875f32256af4d40cc4303ab172
Nisheeth Lahoti <nisheeth.lahoti@gmail.com>
2020-03-03 14:35:54 +0530

    Adding ripgrep, bat and git to packages required to be installed

--- install-config.sh
+++ install-config.sh
@@ -1,8 +1,8 @@
+sudo apt -y install neovim bat ripgrep git
 git clone --bare --config status.showUntrackedFiles=no https://github.com/nisheethlahoti/dotfiles.git ~/.dotfiles.git
 git --git-dir=$HOME/.dotfiles.git/ --work-tree=$HOME checkout
 git --git-dir=$HOME/.dotfiles.git/ config remote.origin.fetch "+refs/heads/*:refs/remotes/origin/*"
 echo "source ~/.additional.bash.rc" >> ~/.bashrc
-sudo apt-get -y install neovim
 curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
 	https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 nvim -c "PlugInstall | qa"
