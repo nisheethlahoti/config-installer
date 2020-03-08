9b578841f15e1138c50efba5fee3630098ef4849
Nisheeth Lahoti <nisheeth.lahoti@gmail.com>
2020-03-08 14:39:34 +0530

    Adding support for .additional.gitconfig

--- install-config.sh
+++ install-config.sh
@@ -5,6 +5,7 @@ sudo apt -y install bat git neovim ripgrep zsh
 git clone --bare --config status.showUntrackedFiles=no https://github.com/nisheethlahoti/dotfiles.git ~/.dotfiles.git
 git --git-dir=$HOME/.dotfiles.git/ --work-tree=$HOME checkout
 git --git-dir=$HOME/.dotfiles.git/ config remote.origin.fetch "+refs/heads/*:refs/remotes/origin/*"
+git config --global --add include.path .additional.gitconfig
 curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
 	https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 nvim -c "PlugInstall | qa" - < /dev/null
