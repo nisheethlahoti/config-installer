c78f80f11ec5475738033fb0c3dc284f2a1310dc
Nisheeth Lahoti <nisheeth.lahoti@gmail.com>
2020-03-08 12:32:36 +0530

    Add zsh as default shell
    Add stuff to change chsh without password, if required

--- README.md
+++ README.md
@@ -3,4 +3,4 @@ Installer for the dotfiles repo.
 Run install-config.sh to download and install the repo.
 
 Usually there's no point cloning this repo. Just run the command  
-`wget -O - https://raw.githubusercontent.com/nisheethlahoti/config-installer/master/install-config.sh | bash`
+`wget --no-cache -O - https://raw.githubusercontent.com/nisheethlahoti/config-installer/master/install-config.sh | bash`
--- install-config.sh
+++ install-config.sh
@@ -1,8 +1,22 @@
-sudo apt -y install neovim bat ripgrep git
+set -e
+
+sudo add-apt-repository ppa:neovim-ppa/unstable
+sudo apt -y install bat git neovim ripgrep zsh
 git clone --bare --config status.showUntrackedFiles=no https://github.com/nisheethlahoti/dotfiles.git ~/.dotfiles.git
 git --git-dir=$HOME/.dotfiles.git/ --work-tree=$HOME checkout
 git --git-dir=$HOME/.dotfiles.git/ config remote.origin.fetch "+refs/heads/*:refs/remotes/origin/*"
-echo "source ~/.additional.bash.rc" >> ~/.bashrc
 curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
 	https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-nvim -c "PlugInstall | qa"
+nvim -c "PlugInstall | qa" - < /dev/null
+
+read -ren 1 -p "Do you know your password? [Y/n] " <&2
+if [[ $REPLY =~ ^[Nn]$ ]]
+then
+	sudo cp /etc/pam.d/chsh /etc/pam.d/chsh.bak
+	sed -Ee 's/required(\s+pam_shells\.so)/sufficient\1/' /etc/pam.d/chsh.bak |
+		sudo tee /etc/pam.d/chsh > /dev/null
+	chsh -s /usr/bin/zsh
+	sudo mv /etc/pam.d/chsh.bak /etc/pam.d/chsh
+else
+	chsh -s /usr/bin/zsh
+fi
