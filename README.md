eee4216ba74035631ade6c0bef5bfd376bc78d01
Nisheeth Lahoti <nisheeth.lahoti@gmail.com>
2024-06-21 13:14:13 +0530

    :zap: Merge os-specific files into main file + bugfixes

--- README.md
+++ README.md
@@ -2,8 +2,7 @@
 Installer for the dotfiles repo.  
 Run install-config.sh to download and install the repo.
 
-Usually there's no point cloning this repo. Just run the commands
+Usually there's no point cloning this repo. Just run the command
 ```
-curl -fsSL https://raw.githubusercontent.com/nisheethlahoti/config-installer/master/<your-os>.sh | bash
 curl -fsSL https://raw.githubusercontent.com/nisheethlahoti/config-installer/master/install-config.sh | bash
 ```
--- install-config.sh
+++ install-config.sh
@@ -1,6 +1,46 @@
 # Function to prompt user for yes/no and return with corresponding value
 confirm() { read -ren 1 -p "$1 [Y/n] " <&2 ; ! [[ $REPLY =~ ^[Nn]$ ]]; }
 
+# OS-specific stuff
+case $(uname -s) in
+	Linux)
+		# Install system packages
+		NVIM_PATH=/usr/bin/nvim
+		case $(cat /etc/os-release | grep "^ID=" | sed "s/ID=//") in
+			ubuntu)
+				sudo apt -y install gcc git ripgrep zsh &&
+				sudo snap install --classic nvim &&
+				NVIM_PATH=/snap/nvim/current/usr/bin/nvim;;
+			fedora) sudo dnf -y install gcc git ripgrep zsh nvim;;
+			arch) sudo pacman --noconfirm -S gcc git ripgrep zsh nvim;;
+			*) echo "Unrecognized linux flavor" && false
+		esac || { echo "Unable to install packages. Aborting" && exit 1; }
+
+		# [Optional] set neovim as default editor
+		sudo update-alternatives --install /usr/bin/editor editor $NVIM_PATH 30
+		if confirm "Set neovim to default editor?"; then
+			sudo update-alternatives --set editor $NVIM_PATH &&
+			echo "Neovim is now the default editor."
+		fi
+
+		# [Optional] set zsh as default shell
+		if confirm "Set zsh to default shell?"; then
+			sudo usermod -s /usr/bin/zsh $USER && echo "Zsh is now the default shell."
+		fi ;;
+	Darwin)
+		# Install homebrew
+		curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh | bash
+
+		# Install packages
+		brew install --cask docker stats
+		brew install iterm2 ripgrep docker-compose neovim
+
+		# Install iterm shell integration and remove zshrc (so it can be loaded later)
+		curl -fsSL https://iterm2.com/shell_integration/install_shell_integration.sh | bash
+		rm ~/.zshrc ;;
+	*) echo "Unrecognized OS. Aborting" && exit 1 ;;
+esac
+
 git config --global --add include.path .additional.gitconfig
 
 # Clone and checkout dotfiles
@@ -9,6 +49,7 @@ git clone --bare --config status.showUntrackedFiles=no\
 git --git-dir=$HOME/.dotfiles.git/ --work-tree=$HOME checkout &&
 git --git-dir=$HOME/.dotfiles.git/ config remote.origin.fetch "+refs/heads/*:refs/remotes/origin/*" &&
 git --git-dir=$HOME/.dotfiles.git/ config branch.master.remote origin &&
+git --git-dir=$HOME/.dotfiles.git/ config branch.master.merge refs/heads/master &&
 echo "All config files downloaded and checked out" || echo "Unable to checkout config files"
 
 # Install neovim plugins
@@ -21,5 +62,8 @@ zsh <(curl -L micro.mamba.pm/install.sh)  # Install micromamba
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/master/install.sh | bash  # Install nvm
 ssh-keygen -t ed25519 # Generate ssh keypair
 
+# Remove changes done to zshrc etc. on plugin installation
+git --git-dir=$HOME/.dotfiles.git/ --work-tree=$HOME checkout HEAD -- $HOME
+
 # Do the rest in a new zsh session
-zsc -c "micromamba install python isort black ; nvm install stable ; npm install -g pyright"
+zsh -c "micromamba install python isort black ; nvm install stable ; npm install -g pyright"
