3e2fc5e10f11fa84b2a345ba76940c7dbeee752d
Nisheeth Lahoti <nisheeth.lahoti@gmail.com>
2024-10-02 08:33:57 +0530

    :zap: Move stuff around + install iterm shell integration regardless of system

--- install-config.sh
+++ install-config.sh
@@ -4,13 +4,16 @@ case $(uname -s) in
 		# Install system packages
 		case $(cat /etc/os-release | grep "^ID=" | sed "s/ID=//") in
 			ubuntu)
-				sudo apt -y install gcc git ripgrep zsh trash-cli &&
+				sudo apt -y install gcc git ripgrep zsh trash-cli atuin &&
 				sudo snap install --classic nvim &&
 				curl -sSL https://pdm-project.org/install-pdm.py | python3 - ;;
 			fedora)
 				sudo dnf -y install gcc git ripgrep zsh neovim trash-cli &&
-				curl -sSL https://pdm-project.org/install-pdm.py | python3 - ;;
-			arch) sudo pacman --noconfirm -S gcc git ripgrep zsh neovim python-pdm htop tmux pkgfile trash-cli atuin unzip;;
+				curl -sSL https://pdm-project.org/install-pdm.py | python3 - &&
+				curl --proto '=https' --tlsv1.2 -LsSf https://setup.atuin.sh | sh;;  # Install atuin
+			arch)
+				sudo pacman --noconfirm -S gcc git ripgrep zsh neovim python-pdm htop tmux pkgfile trash-cli atuin unzip nvidia-open &&
+				sudo pkgfile --update;;
 			*) echo "Unrecognized linux flavor" && false
 		esac || { echo "Unable to install packages. Aborting" && exit 1; }
 
@@ -22,15 +25,15 @@ case $(uname -s) in
 
 		# Install packages
 		brew install --cask docker stats
-		brew install iterm2 ripgrep docker-compose neovim pdm atuin
-
-		# Install iterm shell integration and remove zshrc (so it can be loaded later)
-		curl -fsSL https://iterm2.com/shell_integration/install_shell_integration.sh | bash
-		rm ~/.zshrc ;;
+		brew install iterm2 ripgrep docker-compose neovim pdm atuin;;
 	*) echo "Unrecognized OS. Aborting" && exit 1 ;;
 esac
 
+# Install iterm shell integration
+curl -fsSL https://iterm2.com/shell_integration/install_shell_integration.sh | bash
+
 git config --global --add include.path .additional.gitconfig
+zsh <(curl -L micro.mamba.pm/install.sh)  # Install micromamba
 
 # Clone and checkout dotfiles
 git clone --no-checkout --config status.showUntrackedFiles=no\
@@ -38,15 +41,10 @@ git clone --no-checkout --config status.showUntrackedFiles=no\
 mv ~/.temp/.git ~/.dotfiles.git &&
 rm -d ~/.temp &&
 git --git-dir=$HOME/.dotfiles.git/ config core.worktree ~ &&
-git --git-dir=$HOME/.dotfiles.git/ checkout &&
+git --git-dir=$HOME/.dotfiles.git/ checkout HEAD -- ~ &&
 echo "All config files downloaded and checked out" || echo "Unable to checkout config files"
 
-zsh <(curl -L micro.mamba.pm/install.sh)  # Install micromamba
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/master/install.sh | bash  # Install nvm
-ssh-keygen -t ed25519 # Generate ssh keypair
-
-# Remove changes done to zshrc etc. on plugin installation
-git --git-dir=$HOME/.dotfiles.git/ --work-tree=$HOME checkout HEAD -- $HOME
 
 # Do the rest in a new zsh session
 zsh -c "micromamba install python pynvim ; nvm install stable"
